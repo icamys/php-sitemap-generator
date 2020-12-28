2d16ebd4e5ecc6ff31c63b307525d21f0d6aec7c
icamys <prisacari.dmitrii@gmail.com>
2020-12-29 01:43:39 +0200

    added runtime test

--- test/RuntimeTest.php
+++ test/RuntimeTest.php
@@ -20,10 +20,21 @@ class RuntimeTest extends TestCase
      */
     private $extensionLoadedSpy;
 
+    /**
+     * @var Spy for is_writable function
+     */
+    private $isWritableSpy;
+
     public function testExtensionLoadedCall() {
         $this->r->extension_loaded('curl');
         $this->assertCount(1, $this->extensionLoadedSpy->getInvocations());
         $this->assertEquals('curl', $this->extensionLoadedSpy->getInvocations()[0]->getArguments()[0]);
+    }
+
+    public function testIsWritableCall() {
+        $this->r->is_writable('path');
+        $this->assertCount(1, $this->isWritableSpy->getInvocations());
+        $this->assertEquals('path', $this->isWritableSpy->getInvocations()[0]->getArguments()[0]);
     }
 
     /**
@@ -34,12 +45,14 @@ class RuntimeTest extends TestCase
         $this->r = new Runtime();
         $this->extensionLoadedSpy = new Spy(__NAMESPACE__, "extension_loaded", function (){});
         $this->extensionLoadedSpy->enable();
+        $this->isWritableSpy = new Spy(__NAMESPACE__, "is_writable", function (){});
+        $this->isWritableSpy->enable();
     }
 
     protected function tearDown(): void
     {
         unset($this->r);
         $this->extensionLoadedSpy->disable();
-        $this->extensionLoadedSpy->disable();
+        $this->isWritableSpy->disable();
     }
 }
