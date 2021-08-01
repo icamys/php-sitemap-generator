bb3e68b879c4ac3de37c0afb816feb7369a8e120
icamys <prisacari.dmitrii@gmail.com>
2021-08-01 18:04:58 +0300

    added license file
    Signed-off-by: icamys <prisacari.dmitrii@gmail.com>

--- src/SitemapGenerator.php
+++ src/SitemapGenerator.php
@@ -100,7 +100,7 @@ class SitemapGenerator
      * @var string
      * @access private
      */
-    private $classVersion = "4.3.2";
+    private $classVersion = "4.3.3";
     /**
      * Search engines URLs
      * @var array of strings
