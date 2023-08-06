236d8e7de28e9bb3ff028180ab9e3d0fe7c047b8
Dmitrii Prisacari <prisacari.dmitrii@gmail.com>
2023-08-06 23:25:38 +0100

    chore: bump up generator version

--- src/SitemapGenerator.php
+++ src/SitemapGenerator.php
@@ -106,7 +106,7 @@ class SitemapGenerator
      * @var string
      * @access private
      */
-    private $classVersion = "4.6.0";
+    private $classVersion = "4.7.0";
     /**
      * Search engines URLs
      * @var array of strings
